38c15fda4da0de937d9bf2d4296e7d9d8ee2f01d
dhiraj87shiraskar <dhirajkumarshiraskar@gmail.com>
2023-11-13 12:59:22 +0530

    initial stup commit

---
+++ about-profile/src/app/pages/single-post/single-post.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-single-post',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './single-post.component.html',
+  styleUrl: './single-post.component.css'
+})
+export class SinglePostComponent {
+
+}
---
+++ about-profile/src/app/pages/terms-and-conditions/terms-and-conditions.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-terms-and-conditions',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './terms-and-conditions.component.html',
+  styleUrl: './terms-and-conditions.component.css'
+})
+export class TermsAndConditionsComponent {
+
+}
