aaaca37abaf09f27e4eba3b560cad4739dedd806
dhiraj87shiraskar <dhirajkumarshiraskar@gmail.com>
2023-11-18 13:33:33 +0530

    initial commit

--- ang-blog-dashboard/src/app/app.component.spec.ts
+++ ang-blog-dashboard/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ang-blog-dashboard' title`, () => {
+  it(`should have as title 'ang-blog-dashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ang-blog-dashboard');
---
+++ ang-blog-dashboard/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment: {} = {
+    production: false,
+    firbaseConfig: {
+        apiKey: "AIzaSyDwPVZuiZzJYNtEBwkQ6VDNPDWCayXbexE",
+        authDomain: "ang-blog-31091.firebaseapp.com",
+        projectId: "ang-blog-31091",
+        storageBucket: "ang-blog-31091.appspot.com",
+        messagingSenderId: "521102508599",
+        appId: "1:521102508599:web:32fa0f8302e14f0a1b35b2"
+    }
+};
